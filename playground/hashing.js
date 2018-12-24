04218c9abf05e6704ef1b8b882d079159431db8b
Aashish Biradar <aashish.biradar@gmail.com>
2018-12-24 23:39:01 +0530

    has user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,5 +1,6 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 /***** hashing with crypto-js ******/
 
 var msg = "Message To Be Hashed";
@@ -34,3 +35,32 @@ var jwtToken = jwt.sign(data,'some-salt');
 console.log(`JWT Token: ${jwtToken}`);
 var decoded = jwt.verify(jwtToken,'some-salt');
 console.log(`Decoded: ${JSON.stringify(decoded)}`);
+
+
+/***** hashing with bcryptjs ******/
+
+var password = 'password@123';
+
+bcrypt.genSalt(10,(err,salt)=>{
+    if(err)
+    {
+        throw err;
+    }
+    bcrypt.hash(password,salt,(err,hash) =>{
+        if(err)
+        {
+            throw err;
+        }
+        console.log(hash);
+    });
+});
+
+var hashedPassword = '$2a$10$/DJxWdHZWRs7o8ST2eDSIuulFutidCtozinrZHd1ApWPycaEqEb.e';
+
+bcrypt.compare(password,hashedPassword,(err,res)=>{
+    if(!err)
+    {
+        return console.log(res);
+    }
+    return console.log(err);
+});
